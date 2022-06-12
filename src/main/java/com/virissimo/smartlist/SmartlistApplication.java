e6bc364636e8b865d1956b7e38ab08d4cdeafb19
Francisco Virissimo <virissimo.francisco@gmail.com>
2022-06-12 13:43:19 -0300

    Inicializando projeto springboot

---
+++ src/main/java/com/virissimo/smartlist/SmartlistApplication.java
@@ -0,0 +1,13 @@
+package com.virissimo.smartlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virissimo/smartlist/SmartlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virissimo.smartlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
